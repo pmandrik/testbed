7468b384401bd441e2332052efb5fc196977d4b4
pmandrik <petr.mandrik@cern.ch>
2023-03-03 18:49:59 +0300

    add template 0 cpp contests programming

---
+++ 5_cppcontests/template_0.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int Tt, N;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin >> Tt;
+
+  unsigned long long int answer;
+
+  for(int t=0;t<Tt;t++) {
+    cin >> N;
+
+    else printf( "%llu\n", answer );
+  }
+
+  return 0;
+}
+
+void problem_C(){
+  ifstream in("input.txt");
+  cin.rdbuf(in.rdbuf());
+  main();
+}
+    
+       
